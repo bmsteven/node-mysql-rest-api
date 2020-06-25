4c32912334ddc038ddb69559cb77d196ab5406cb
Benedict <bmsteve96@gmail.com>
2020-06-25 12:37:10 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const app = require("express")()
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))

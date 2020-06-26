4b75b3f39709ab28904fa2645881c85d03671e5f
Benedict <bmsteve96@gmail.com>
2020-06-27 00:07:46 +0300

    added mysql connection

--- index.js
+++ index.js
@@ -1,8 +1,50 @@
+const path = require("path")
+
+const bodyParser = require("body-parser");
+
+const db = require("./src/models/db")
+
 const app = require("express")()
 
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// landing page of the api
 app.get("/", (req, res) => {
-    res.send("Hello world")
-})
+  res.sendFile(path.join(__dirname, "views", "index.html"))
+});
+
+//create a database, when running this route, mae sure you comment the database name in mysql.createConnection method
+// app.get("/createdb", (req, res) => {
+//   let sql = "Create database niuzieApp"
+
+//   db.query(sql, (err, result) => {
+//     if (err) throw err
+//     res.json(result)
+//   })
+// })
+
+//create seller table
+// app.get("/createSellerTable", (req, res) => {
+//   let sql = "Create table sellers(id varchar(36) Primary key, firstName varchar(255), lastName varchar(255), email varchar(255), tel varchar(255) )"
+
+//   db.query(sql, (err, result) => {
+//     if (err) throw err
+//     res.json(result)
+//   })
+// })
+
+// app.get("/createProductsTable", (req, res) => {
+//   let sql = "create table products()"
+
+//   db.query(sql, (err, result) => {
+//     if (err) throw err
+//     res.json(result)
+//   })
+// })
 
 const PORT = process.env.PORT || 5000
 
---
+++ src/models/db.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql")
+
+const dbConfig = require("../config/db.config")
+
+const db = mysql.createConnection({
+    host : process.env.DB_HOST || dbConfig.host,
+    user: process.env.DB_USER || dbConfig.user,
+    password: process.env.DB_PASS || dbConfig.password,
+    database: process.env.DB_DATABASE || dbConfig.database
+})
+
+db.connect((err) => {
+    if (err) throw err
+    console.log("Database connected...")
+})
+
+module.exports = db

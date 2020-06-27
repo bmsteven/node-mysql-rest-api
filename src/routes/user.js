9884fbbef877fce69d02fd45efa3e5069fd8f4ef
Benedict <bmsteve96@gmail.com>
2020-06-27 03:57:04 +0300

    wored on registration route

--- src/routes/user.js
+++ src/routes/user.js
@@ -11,13 +11,13 @@ let hash = bcrypt.hashSync( "B4c0/\/", salt );
 
 exports.register = async ( req, res ) => {
 
+    //gets password from the client
     let pass = req.body.password
 
-    // let isEmail = false
-    // let isTel = false
-
+    //hashes password
     let hashedpassword = await bcrypt.hash( pass, salt );
 
+    // setting new user data
     let newUser = {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -26,36 +26,26 @@ exports.register = async ( req, res ) => {
         password: hashedpassword
     }
 
+    //destructuring data
     const { firstName, lastName, email, tel, password } = newUser
 
     let emailCheck = "SELECT email from sellers where email = '" + email + "'"
     let telCheck = "SELECT * from sellers where tel = '" + tel + "'"
 
-    // db.query(emailCheck, (err, results) => {
-    //     if (err) throw err
-    //     console.log(results)
-    //     if (results.length > 0) isEmail = true
-    // })
-
-    // db.query(telCheck, (err, results) => {
-    //     if (err) throw err
-    //     console.log(results)
-    //     if (results > 0) isTel = true
-    // })
-
-    // console.log(isEmail, isTel)
-
+    //checks if email exists
     db.query( emailCheck, ( err, results ) => {
         if ( err ) throw err
         if ( results.length > 0 ) {
             res.status( 400 ).json( { error: "email address already in use" } )
         }
         else {
+            // checks if telephone exists
             db.query( telCheck, ( err, result ) => {
                 if ( err ) throw err
                 if ( result.length > 0 ) {
                     res.status( 400 ).json( { error: "telephone number already in use" } )
                 } else {
+                    // registering user 
                     let sql = "INSERT INTO `sellers` values (uuid(),?,?,?,?,?)"
                     db.query( sql, [firstName, lastName, email, tel, password], ( err, result ) => {
                         if ( err ) throw err
